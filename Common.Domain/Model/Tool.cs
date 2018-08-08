8d8f912bea9d5534eda1ecc6ea3523cc397b6722
Wilson Santos <wilsonsantosnet@gmail.com>
2018-08-08 08:40:27 -0300

    improvements for Auth

---
+++ Common.Domain/Model/Tool.cs
@@ -0,0 +1,28 @@
+﻿using Common.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Domain.Model
+{
+    public class Tool
+    {
+        public Tool()
+        {
+            this.CanWrite = true;
+            this.CanRead = true;
+            this.CanDelete = true;
+        }
+
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Route { get; set; }
+        public string Key { get; set; }
+        public string ParentKey { get; set; }
+        public ETypeTools Type { get; set; }
+        public Boolean CanWrite { get; set; }
+        public Boolean CanRead { get; set; }
+        public Boolean CanDelete { get; set; }
+
+    }
+}
