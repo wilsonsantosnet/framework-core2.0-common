360440ed3976c98034e093c20772b01a3d411d69
Wilson Santos <wilsonsantosnet@gmail.com>
2020-06-05 12:32:17 -0300

    Update DtoBase.cs

--- Common.Domain/Base/DtoBase.cs
+++ Common.Domain/Base/DtoBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Common.Dto
+namespace Common.Domain.Base
 {
     public abstract class DtoBase
     {
